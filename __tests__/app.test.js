4ac3ed901deb8865e0306226efe87e344494b318
amayamaya <aamaya547@gmail.com>
2022-12-12 14:00:47 -0800

    #GET all Pantones test is failing as expected - model has been started

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,18 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#GET all should return a list of Pantones', async () => {
+    const res = await request(app).get('/api/v1/colors');
+    console.log('hey all Panbtones res', res.body);
+    expect(res.status).toBe(200);
   });
+
   afterAll(() => {
     pool.end();
   });
