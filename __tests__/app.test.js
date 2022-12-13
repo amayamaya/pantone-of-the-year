99bd85c12ad858e8e5e30bd2b2d96678cfa639a1
amayamaya <aamaya547@gmail.com>
2022-12-12 16:15:53 -0800

    #GET single pantone test failing as expected

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,12 +7,25 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
   it('#GET all should return a list of Pantones', async () => {
     const res = await request(app).get('/api/v1/colors');
     console.log('hey all Pantones res', res.body);
     expect(res.status).toBe(200);
   });
 
+  it('#GET by id should return a single Pantone', async () => {
+    const res = await request(app).get('/api/v1/colors/1');
+    console.log('hey single Pantone res', res.body);
+    expect(res.status).toEqual({
+      id: expect.any(String),
+      year: expect.any(Number),
+      name: expect.any(String),
+      value: expect.any(String),
+      hex: expect.any(String),
+    });
+
+  });
   afterAll(() => {
     pool.end();
   });
